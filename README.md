c246e13662bd9a0f0fb3a9e989bde71fb7dcdcd0
Abraham015 <ahernadeza1607@alumno.ipn.mx>
2024-08-28 17:03:52 -0600

    Backend develop

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MovieList
+
 Project using ReactJS, SpringBoot and MongoDB
+
+## Backend
+
+For the backend part I used SpringBoot 3.3.3 and some dependencies such as lombok. For the connection with the database I used MongoDB dependency.
+
+## Frontend
---
+++ movies/src/main/java/dev/abraham/movies/MovieService.java
@@ -0,0 +1,25 @@
+package dev.abraham.movies;
+
+import dev.abraham.movies.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    private final MovieRepository movieRepository;
+    @Autowired
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(String id){
+        return movieRepository.findMovieByImdbId(id);
+    }
+}
